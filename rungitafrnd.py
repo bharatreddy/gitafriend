fe4ad2381530c2f4db2d243db4622e043af6b6ec
Bharat Kunduri <bharatr@vt.edu>
2014-01-30 15:49:28 -0800

    changed utf-8 codec to ISO-8859-1

--- rungitafrnd.py
+++ rungitafrnd.py
@@ -56,7 +56,7 @@ def userpage():
             qryStr = "select name, start_date, nflwr from userdetail where login='"+uu+"';"
             db.query( qryStr )
             query_results = db.store_result().fetch_row( maxrows=0 )
-            nameUserList.append( query_results[0][0].decode('utf-8') )
+            nameUserList.append( query_results[0][0].decode('ISO-8859-1') )
             # convert the datetime format to an appropriate format
             startDateUserList.append( query_results[0][1].strftime('%m/%d/%Y') )
             nFlwrUserList.append( query_results[0][2] )
